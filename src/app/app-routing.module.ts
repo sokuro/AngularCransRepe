2c9280535175ee0a052bbcb2ed6030aace53c5eb
Ugorcak, Karol Sokuro <karol.ugorcak@adesso.ch>
2021-12-04 19:19:20 +0000

    extend the Routing Module with CreateToDo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { CreateToDoComponent } from "./to-do/create-to-do/create-to-do.component";
+import { DeleteToDoComponent } from "./to-do/delete-to-do/delete-to-do.component";
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'ToDo/Create', component: CreateToDoComponent},
+  { path: 'ToDo/Delete/:Id', component: DeleteToDoComponent},
   { path: '**', redirectTo: 'home' }
 ];
 
