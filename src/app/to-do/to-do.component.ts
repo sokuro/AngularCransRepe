3516e0c8eac1e5b341d10c6944302da087999a53
Ugorcak, Karol Sokuro <karol.ugorcak@adesso.ch>
2021-12-04 18:46:16 +0000

    implement the HttpClient into the ToDoComponent

--- src/app/to-do/to-do.component.ts
+++ src/app/to-do/to-do.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { IToDo, ToDo } from "../models/to-do";
+import { Category } from "../models/category";
 
 @Component({
   selector: 'app-to-do',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToDoComponent implements OnInit {
 
-  constructor() { }
+  public ToDoItems: ToDo[] | null = null;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get<IToDo[]>('https://todoapp42.azurewebsites.net/api/todoes') .subscribe(result => {
+      this.ToDoItems = result.map(( receivedData:IToDo) =>{
+        return new ToDo(
+          receivedData.id,
+          receivedData.title,
+          receivedData.date,
+          receivedData.categoryId,
+          new Category(
+            receivedData.category?.id,
+            receivedData.category?.title,
+            receivedData.category?.color ),
+          receivedData.description
+        )
+      } );
+    }, error => console.error(error));
+
   }
 
 }
