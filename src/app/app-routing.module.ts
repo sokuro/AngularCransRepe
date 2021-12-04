20b7c74f391283acd815af26417f33665bd2bf1d
Ugorcak, Karol Sokuro <karol.ugorcak@adesso.ch>
2021-12-04 19:27:32 +0000

    adding the EditToDo to the App's Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { CreateToDoComponent } from "./to-do/create-to-do/create-to-do.component";
 import { DeleteToDoComponent } from "./to-do/delete-to-do/delete-to-do.component";
+import { EditToDoComponent } from "./to-do/edit-to-do/edit-to-do.component";
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'ToDo/Create', component: CreateToDoComponent},
   { path: 'ToDo/Delete/:Id', component: DeleteToDoComponent},
+  { path: 'ToDo/Edit/:Id', component: EditToDoComponent},
   { path: '**', redirectTo: 'home' }
 ];
 
