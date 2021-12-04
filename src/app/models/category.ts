617861554274ed21d5b0f3a17cd504300d2cf842
Ugorcak, Karol Sokuro <karol.ugorcak@adesso.ch>
2021-12-04 17:19:15 +0000

    adding class ToDo and Category

--- src/app/models/category.ts
+++ src/app/models/category.ts
@@ -1,2 +1,40 @@
+export interface ICategory {
+  id: number;
+  title: string;
+  color: string;
+}
+
 export class Category {
+
+  get Id(): number {
+    return this.id;
+  }
+
+  set Id(value) {
+    this.id = value;
+  }
+
+  get Title(): string {
+    return this.title;
+  }
+
+  set Title(value) {
+    this.title = value;
+  }
+
+  get Color() {
+    return this.color;
+  }
+
+  set Color(value) {
+    this.color = value;
+  }
+
+  constructor(
+    private id: number=0,
+    private title: string="",
+    private color: string=""
+  )
+  {
+  }
 }
--- src/app/models/to-do.ts
+++ src/app/models/to-do.ts
@@ -1,5 +1,12 @@
+import { Category, ICategory } from "./category";
+
 export interface IToDo {
   id: number;
+  title: string;
+  date: Date;
+  categoryId: number|null;
+  category: ICategory|null;
+  description: string;
 }
 
 export class ToDo {
@@ -10,8 +17,49 @@ export class ToDo {
     this.id= value;
   }
 
+  get Title(): string {
+    return this.title;
+  }
+  set Title(value: string) {
+    this.title= value;
+  }
+
+  get Date(): Date {
+    return this.date;
+  }
+  set Date(value: Date) {
+    this.date= value;
+  }
+
+  get CategoryId(): number | null {
+    return this.categoryId;
+  }
+  set CategoryId(value: number | null) {
+    this.categoryId= value;
+  }
+
+  get Category(): Category | null {
+    return this.Category;
+  }
+
+  set Category(value) {
+    this.Category = value;
+  }
+
+  get Description(): string {
+    return this.description;
+  }
+  set Description(value: string) {
+    this.description= value;
+  }
+
   constructor(
-    private id: number=0
+    private id: number=0,
+    private title: string,
+    private date: Date=new Date(Date.now()),
+    private categoryId: number | null = null,
+    private category: Category | null = null,
+    private description: string=""
   )
   {
   }
